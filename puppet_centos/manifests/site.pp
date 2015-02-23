137f9828d8b0b755db7307ad1794308daca28cad
Timm-CentOS <timmvt@gmail.com>
2015-02-23 16:29:31 -0500

    Adding all code to manifests/site.pp to get away from using the IMPORT command.

--- puppet_centos/manifests/site.pp
+++ puppet_centos/manifests/site.pp
@@ -1,2 +1,11 @@
-import 'nodes.pp'
+node 'demo' {  # PAGE 30
+  file { '/tmp/hello':
+    content => "Hello, world -nodes\n",
+  }
+}
+
+
+# import 'nodes.pp' :: According to Puppet Labs, the IMPORT command is being deprecated and will not be in use anymore with Puppet 4. [SOURCE: https://docs.puppetlabs.com/puppet/latest/reference/lang_import.html] To get around this while learning Puppet, modules will be used.
+
+
 
