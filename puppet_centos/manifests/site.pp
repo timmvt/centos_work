7723760b20bb3f7e56f6881297329d839703fca6
Timm-CentOS <timmvt@gmail.com>
2015-02-27 11:59:22 -0500

    Updating manifests/site.pp with chain command exec code

--- puppet_centos/manifests/site.pp
+++ puppet_centos/manifests/site.pp
@@ -2,6 +2,33 @@ node 'demo' {
   include httpd
   include sudoers
 
+  exec { 'Run my arbitrary command':
+    command => '/bin/echo I ran this command on `/bin/date` >/tmp/command.output.txt',
+  }
+
+  exec { 'Download public key for John':
+    cwd     => '/tmp',
+    command => '/usr/bin/wget http://bitfieldconsulting.com/files/john.pub',
+    creates => '/tmp/john.pub',
+  }
+
+### TRYING CHAINING COMMAND (not in the Puppet 3 book)
+
+  exec { 'step 1':
+    command => '/bin/echo Hi step 1 run on `/bin/date` >/tmp/testttss.output.txt',
+  }
+  
+  exec { 'step 2':
+    command => '/bin/echo Hi step 2 run on `/bin/date` >/tmp/testtts2.output.txt',
+    require => Exec['step 1'],
+  }
+
+  exec { 'step 3':
+    command => '/bin/echo Hi step 3 run on `/bin/date` >/tmp/testtts3.output.txt',
+    require => Exec['step 2'],
+  }
+
+
 #  file { '/tmp/hello':   # PAGE 30
 #    content => "Hello, world -nodes\n",
 #  }
