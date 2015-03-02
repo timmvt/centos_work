c0a197760dc0de81f9d96425f42521755ee12f51
Timm-CentOS <timmvt@gmail.com>
2015-03-02 11:40:01 -0500

    Updating ssh/manifests/init.pp

--- puppet_centos/modules/ssh/manifests/init.pp
+++ puppet_centos/modules/ssh/manifests/init.pp
@@ -4,5 +4,14 @@ class ssh {
   service { 'sshd':
     ensure => running,
   }
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => file,	# TT ADDED from https://docs.puppetlabs.com/puppet/latest/reference/lang_relationships.html
+    mode    => 600,    # TT ADDED from https://docs.puppetlabs.com/puppet/latest/reference/lang_relationships.html
+    source  => 'puppet:///modules/ssh/sshd_config',
+#    require => Package['openssh-server'],    # TT ADDED from https://docs.puppetlabs.com/puppet/latest/reference/lang_relationships.html
+    notify  => Service['sshd'],
+  }
+
 }
 
